b318dc3c122b517c04ef3ae5500d48ad604e3ca7
murramge <dmdgi37@naver.com>
2024-02-27 19:01:15 +0900

    프로젝트기초공사1

--- src/App.js
+++ src/App.js
@@ -5,17 +5,50 @@ import Edit from "./pages/Edit";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import RouteTest from "./components/Routetest";
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton
+              text={"왼쪽 버튼"}
+              onClick={() => alert("왼쪽 클릭")}></MyButton>
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}></MyButton>
+          }></MyHeader>
         <h2>App.js</h2>
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}></MyButton>
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}></MyButton>
+        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")}></MyButton>
+
+        {/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} /> */}
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/new" element={<New />}></Route>
           <Route path="/edit" element={<Edit />}></Route>
-          <Route path="/diary" element={<Diary />}></Route>
+          <Route path="/diary/:id" element={<Diary />}></Route>
         </Routes>
         <RouteTest />
       </div>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,15 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+
   return (
     <div>
-      <h1>Edit</h1>
+      <button onClick={() => setSearchParams({ who: "12222" })}>바꾸기</button>
+      <button onClick={() => navigate(-1)}>뒤로가기</button>
     </div>
   );
 };
